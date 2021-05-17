aec612c008f01159cc32f97b4cf91d9812a6ff2a
Nemanja Pavlovic <pavlovicn88@gmail.com>
2021-05-17 12:17:11 +0200

    Swagger UI and documentation is ready .

--- src/main/java/com/tracker/Tournament/TournamentApplication.java
+++ src/main/java/com/tracker/Tournament/TournamentApplication.java
@@ -2,10 +2,10 @@ package com.tracker.Tournament;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-
+@EnableSwagger2
 public class TournamentApplication {
 
 	public static void main(String[] args) {
